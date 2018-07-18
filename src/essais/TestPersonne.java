e7c53c92cb82478a821c1b0323d293377fbec096
Dylan <dupont.cail@gmail.com>
2018-07-18 10:54:30 +0200

    Tp numero 1

---
+++ src/essais/TestPersonne.java
@@ -0,0 +1,21 @@
+package essais;
+
+import entites.AdressePostale;
+import entites.Personne;
+
+public class TestPersonne {
+
+	public static void main(String[] args) {
+		Personne b = new Personne();
+		b.nom = "Djamel";
+		b.prenom = "Djamel";
+		b.adresse = new AdressePostale();
+		b.adresse.numeroRue = 5;
+		b.adresse.libelleRue = "rue de ..";
+		b.adresse.ville = "Nantes";
+		
+				
+
+	}
+
+}
